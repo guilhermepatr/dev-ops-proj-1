851d5dddd13963d19b75b74340ffb837ef1aa266
guilhermepatr <guilhermeps96@hotmail.com>
2024-11-23 23:33:39 -0300

    atualizando index

--- index.php
+++ index.php
@@ -25,52 +25,58 @@
                         <a href="create.php" class="btn-custom"><i class="fa fa-plus"></i> Adicionar Novo Insumo</a>
                     </div>
 
-                    <?php
-                    // Inclui o arquivo de configuração
-                    require_once "config.php";
+<?php
+// Inclui o arquivo de configuração
+require_once "config.php";
 
-                    // Executa a consulta para obter os insumos
-                    $sql = "SELECT * FROM insumos";
-                    if($result = mysqli_query($link, $sql)){
-                        if(mysqli_num_rows($result) > 0){
-                            echo '<table class="table table-bordered table-striped">';
-                                echo "<thead>";
-                                    echo "<tr>";
-                                        echo "<th>ID</th>";
-                                        echo "<th>Nome do Produto</th>";
-                                        echo "<th>Tipo do Material</th>";
-                                        echo "<th>Quantidade</th>";
-                                        echo "<th>Valor</th>";
-                                        echo "<th>Ação</th>";
-                                    echo "</tr>";
-                                echo "</thead>";
-                                echo "<tbody>";
-                                while($row = mysqli_fetch_array($result)){
-                                    echo "<tr>";
-                                        echo "<td>" . $row['id'] . "</td>";
-                                        echo "<td>" . $row['nome_produto'] . "</td>";
-                                        echo "<td>" . $row['tipo_material'] . "</td>";
-                                        echo "<td>" . $row['quantidade'] . "</td>";
-                                        echo "<td>R$ " . number_format($row['valor'], 2, ',', '.') . "</td>";
-                                        echo "<td>";
-                                            echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="Visualizar" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
-                                            echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Editar" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
-                                            echo '<a href="delete.php?id='. $row['id'] .'" title="Excluir" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
-                                        echo "</td>";
-                                    echo "</tr>";
-                                }
-                                echo "</tbody>";                            
-                            echo "</table>";
-                            mysqli_free_result($result);
-                        } else{
-                            echo '<div class="alert alert-danger"><em>Nenhum registro encontrado.</em></div>';
-                        }
-                    } else{
-                        echo "Erro ao tentar executar a consulta.";
-                    }
+// Executa a consulta para obter os insumos
+$sql = "SELECT * FROM insumos";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo '<table class="table table-bordered table-striped">';
+            echo "<thead>";
+                echo "<tr>";
+                    echo "<th>ID</th>";
+                    echo "<th>Nome do Produto</th>";
+                    echo "<th>Tipo do Material</th>";
+                    echo "<th>Quantidade</th>";
+                    echo "<th>Valor</th>";
+                    echo "<th>Ação</th>";
+                echo "</tr>";
+            echo "</thead>";
+            echo "<tbody>";
+            while($row = mysqli_fetch_array($result)){
+                echo "<tr>";
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . $row['nome'] . "</td>"; // Corrigido para 'nome'
+                    echo "<td>" . $row['tipo'] . "</td>"; // Corrigido para 'tipo'
+                    echo "<td>" . $row['quantidade'] . "</td>";
+                    echo "<td>R$ " . number_format($row['valor'], 2, ',', '.') . "</td>";
+                    echo "<td>";
+                        echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="Visualizar" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                        echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Editar" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                        echo '<a href="delete.php?id='. $row['id'] .'" title="Excluir" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                    echo "</td>";
+                echo "</tr>";
+            }
+            echo "</tbody>";                            
+        echo "</table>";
+        mysqli_free_result($result);
+    } else{
+        echo '<div class="alert alert-danger"><em>Nenhum registro encontrado.</em></div>';
+    }
+} else{
+    echo "Erro ao tentar executar a consulta.";
+}
 
-                    mysqli_close($link);
-                    ?>
+mysqli_close($link);
+?>
+
+
+
+
+
+
                 </div>
             </div>        
         </div>
